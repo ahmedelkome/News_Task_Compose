069c26a189808afcec2b1d1e2d77b682d17d6e30
ahmed elkomy <ahmedelkomy@gmail.com>
2024-09-03 01:42:53 +0300

    create data class Article for hold data

---
+++ domain/src/main/java/com/route/domain/model/Article.kt
@@ -0,0 +1,11 @@
+package com.route.domain.model
+
+data class Article (
+    val title:String,
+    val description:String,
+    val url:String,
+    val urlImage:String,
+    val publishAt:String,
+    val content:String
+){
+}

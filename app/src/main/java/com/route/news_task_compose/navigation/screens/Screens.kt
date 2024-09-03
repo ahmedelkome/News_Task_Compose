65a7140ecda6b838c710db66eb6723918f401dd4
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-09-03 03:19:54 +0300

    create class Screens to route

---
+++ app/src/main/java/com/route/news_task_compose/navigation/screens/Screens.kt
@@ -0,0 +1,16 @@
+package com.route.news_task_compose.navigation.screens
+
+sealed class Screens(val route: String) {
+    data object SplashScreen : Screens("splash")
+    data object HomeScreen : Screens("home")
+
+    fun withArgs(vararg args: String?): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+
+    }
+}

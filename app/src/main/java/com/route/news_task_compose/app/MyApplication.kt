2acb8c13f8d077cfc3cf2f5623bae53ba22cace4
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-09-04 09:32:41 +0300

    create class for check internet

--- app/src/main/java/com/route/news_task_compose/app/MyApplication.kt
+++ app/src/main/java/com/route/news_task_compose/app/MyApplication.kt
@@ -1,8 +1,13 @@
 package com.route.news_task_compose.app
 
 import android.app.Application
+import com.route.data.utils.ConnectivityChecker
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        ConnectivityChecker.context = this
+    }
 }
